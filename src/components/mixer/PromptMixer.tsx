129c1050d5f0559f3d01bdb73b69597d5238f3d9
Ivan Charapanau <mail@av.codes>
2026-02-09 16:31:11 +0100

    fix: update overflow properties and add padding in PromptMixer component for improved layout

--- src/components/mixer/PromptMixer.tsx
+++ src/components/mixer/PromptMixer.tsx
@@ -6,21 +6,21 @@ import '../../styles/mixer.css';
 
 export function PromptMixer() {
   return (
-    <div className="flex flex-col h-full overflow-hidden">
+    <div className="flex flex-col h-full overflow-visible pb-1">
       {/* Main Canvas Panel - Full Height */}
-      <div className="nc-panel flex-grow relative flex flex-col">
+      <div className="nc-panel flex-grow relative flex flex-col overflow-visible"> {/** Changed overflow-hidden to overflow-visible and added padding-bottom 1px */}
         <div className="nc-header">CANVAS_MIXER</div>
         <div className="flex items-center justify-between px-2 pt-4 pb-2 border-b border-dashed border-white">
           <MixerControls />
           <div className="text-[var(--nc-yellow)] text-sm">PRESETS: [DEFAULT]</div>
         </div>
-        <div className="flex-grow relative">
+        <div className="flex-grow relative overflow-hidden">
           <MixerCanvas />
         </div>
       </div>
 
       {/* Floating Weights Panel - Bottom Strip */}
-      <div className="nc-panel h-1/2 relative flex flex-col">
+      <div className="nc-panel h-1/2 relative flex flex-col overflow-visible mt-2">
         <div className="nc-header">ANCHOR_WEIGHTS</div>
         <ValueDisplay />
       </div>
