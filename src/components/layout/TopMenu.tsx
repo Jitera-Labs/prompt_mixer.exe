58db36e76b46151612104a0102bb877bf83f40f4
Ivan Charapanau <mail@av.codes>
2026-02-10 15:04:43 +0100

    refactor: integrate toggleSettings functionality into TopMenu component

--- src/components/layout/TopMenu.tsx
+++ src/components/layout/TopMenu.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useSettingsStore } from '../../stores/settingsStore';
+
 
 /**
  * TopMenu - Norton Commander style top menu bar
  * Features: File/Edit/Search/Options/Help menu items, app title, settings icon
  */
 const TopMenu: React.FC = () => {
+  const toggleSettings = useSettingsStore((state) => state.toggleSettings);
+
   return (
     <header className="bg-[var(--nc-cyan)] text-black border-b-2 border-black px-4 py-2 flex justify-between items-center">
       {/* Left: Menu Items */}
@@ -18,7 +22,7 @@ const TopMenu: React.FC = () => {
         <button className="hover:opacity-80">
           <span className="underline decoration-2 underline-offset-2 decoration-red-900">S</span>earch
         </button>
-        <button className="hover:opacity-80">
+        <button className="hover:opacity-80" onClick={toggleSettings}>
           <span className="underline decoration-2 underline-offset-2 decoration-red-900">O</span>ptions
         </button>
         <button className="hover:opacity-80">
@@ -32,7 +36,7 @@ const TopMenu: React.FC = () => {
       </div>
 
       {/* Right: Settings Icon */}
-      <button className="cursor-pointer hover:opacity-80" aria-label="Settings">
+      <button className="cursor-pointer hover:opacity-80" aria-label="Settings" onClick={toggleSettings}>
         <span className="material-symbols-outlined text-3xl">settings</span>
       </button>
     </header>
